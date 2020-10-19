a530bd1f811ce1372b2dfab396345e778d61f84a
Marina <maryna.voloshyna.11@gmail.com>
2020-10-19 11:18:58 +0300

    with finally and try with resources

--- src/main/java/spring/dao/impl/UserDaoImpl.java
+++ src/main/java/spring/dao/impl/UserDaoImpl.java
@@ -1,10 +1,13 @@
 package spring.dao.impl;
 
 import java.util.List;
-import javax.persistence.TypedQuery;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import spring.dao.UserDao;
+import spring.exceptions.DataProcessingException;
 import spring.model.User;
 
 @Repository
@@ -17,13 +20,34 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void add(User user) {
-        sessionFactory.openSession().save(user);
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert user Entity with email = "
+                   + user.getEmail(), e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<User> listUsers() {
-        TypedQuery<User> query = sessionFactory.openSession()
-                .createQuery("from User", User.class);
-        return query.getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = sessionFactory.openSession()
+                    .createQuery("from User", User.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all users", e);
+        }
     }
 }
